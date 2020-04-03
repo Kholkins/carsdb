92d80abb696c3351429a6f3ff5ea862611ffbd0f
Kholkin <kholkins@mail.ru>
2020-04-03 20:56:41 +0300

    CRUD: create

--- app/src/main/java/Data/DatabaseHandler.java
+++ app/src/main/java/Data/DatabaseHandler.java
@@ -1,11 +1,11 @@
 package Data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
-
+import Model.Car;
 import Utils.Util;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -27,4 +27,16 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS "+Util.TABLE_NAME);
         onCreate(db);
     }
+
+    public void addCar(Car car){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Util.KEY_NAME, car.getName());
+        contentValues.put(Util.KEY_PRICE, car.getPrice());
+
+        db.insert(Util.TABLE_NAME,null,contentValues);
+        db.close();
+    }
+
 }
