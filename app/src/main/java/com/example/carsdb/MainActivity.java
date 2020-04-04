a653d603fd91a2a2a5af3f8c8d5e11adb0dbcb45
Kholkin <kholkins@mail.ru>
2020-04-04 20:46:47 +0300

    CRUD: read all

--- app/src/main/java/com/example/carsdb/MainActivity.java
+++ app/src/main/java/com/example/carsdb/MainActivity.java
@@ -18,15 +18,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         DatabaseHandler databaseHandler = new DatabaseHandler(this);
+        Log.d("CarsCount:", String.valueOf(databaseHandler.getCarsCount()));
 
-        databaseHandler.addCar(new Car("Toyota", "30000"));
-        databaseHandler.addCar(new Car("Mercedes", "50000"));
-        databaseHandler.addCar(new Car("Opel", "25000"));
-        databaseHandler.addCar(new Car("Kia", "31000"));
+        /*databaseHandler.addCar(new Car("Toyota", "30 000 $"));
+        databaseHandler.addCar(new Car("Opel", "25 000 $"));
+        databaseHandler.addCar(new Car("Mercedes", "50 000 $"));
+        databaseHandler.addCar(new Car("KIA", "28 000 $"));
+        databaseHandler.addCar(new Car("Mazda", "30 000 $"));
+        databaseHandler.addCar(new Car("Honda", "25 000 $"));
+        databaseHandler.addCar(new Car("Skoda", "50 000 $"));
+        databaseHandler.addCar(new Car("Hundai", "28 000 $"));*/
 
-        List<Car> listCars = databaseHandler.getAllCars();
-        for (Car car: listCars){
-            Log.d("car: ", "id "+car.getId()+", name "+car.getName()+", price "+ car.getPrice());
+        List<Car> carList = databaseHandler.getAllCars();
+
+
+
+        /*Car deletingCar = databaseHandler.getCar(7);
+        databaseHandler.deleteCar(deletingCar);*/
+
+        for (Car car : carList) {
+            Log.d("CarInfo:", "ID " + car.getId() + ", name " + car.getName() +
+                    ", price " + car.getPrice());
         }
+
+        /*Car car = databaseHandler.getCar(1);
+        car.setName("Tesla");
+        car.setPrice("50 000 $");
+        int updatedCarId = databaseHandler.updateCar(car);
+        Log.d("CarInfo:", "ID " + car.getId() + ", name " + car.getName() +
+                ", price " + car.getPrice() + ", updatedCarId " + updatedCarId);*/
     }
 }
